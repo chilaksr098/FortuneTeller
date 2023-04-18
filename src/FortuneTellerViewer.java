c155e9bd341e11657ec1f3bd8013cb19be181625
Sasidhar Chilaka <sasi@Sasidhars-MacBook-Pro-5.local>
2023-04-18 04:10:34 +0400

    Initial commit

---
+++ src/FortuneTellerViewer.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class FortuneTellerViewer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        JFrame frame = new FortuneTellerFrame();
+        frame.setVisible(true);
+    }
+
+}
